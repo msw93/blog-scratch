8a7cb969719df6175928d3f16432f8c0abae56f0
mikewiner <winer.mike@gmail.com>
2020-08-30 15:52:06 -0400

    added icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,19 @@ module.exports = {
   plugins: [
     `gatsby-plugin-emotion`,
     `gatsby-transformer-remark`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Mike W Blog`,
+        short_name: `MW-Blog`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `src/images/icon/thik-logo.svg`,
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
